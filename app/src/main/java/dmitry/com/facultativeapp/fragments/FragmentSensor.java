f25588c387275d95a18aa263997a2d1b3978f06e
AlwaysOnThePositive <43723172+AlwaysOnThePositive@users.noreply.github.com>
2018-12-13 16:22:23 +0300

    Сделано сохранение фотки

--- app/src/main/java/dmitry/com/facultativeapp/fragments/FragmentSensor.java
+++ app/src/main/java/dmitry/com/facultativeapp/fragments/FragmentSensor.java
@@ -1,23 +1,36 @@
 package dmitry.com.facultativeapp.fragments;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.Objects;
 import java.util.Timer;
 
 import dmitry.com.facultativeapp.R;
+import dmitry.com.facultativeapp.helpers.Instruments;
 
 public class FragmentSensor extends Fragment {
 
@@ -26,26 +39,23 @@ public class FragmentSensor extends Fragment {
     private Sensor sensor;
     private Timer timer;
     private StringBuilder sb = new StringBuilder();
+    private Bitmap b;
 
     private Button btnTakePhoto;
     private Button btnSavePhoto;
+    private ImageView imgViewScreen;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View v = inflater.inflate(R.layout.fragment_sensor, null);
+        View v = inflater.inflate(R.layout.fragment_sensor, container, false);
 
         tvAccelerometer = v.findViewById(R.id.tvAccelerometer);
         btnSavePhoto = v.findViewById(R.id.btnSavePhoto);
         btnTakePhoto = v.findViewById(R.id.btnTakePhoto);
-
-        btnTakePhoto.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
+        imgViewScreen = v.findViewById(R.id.imgViewScreen);
 
         sensorManager = (SensorManager) (Objects.requireNonNull(getActivity())).getSystemService
                 (Context.SENSOR_SERVICE);
@@ -53,6 +63,74 @@ public class FragmentSensor extends Fragment {
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
         return v;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // Check the SDK version and whether the permission is already granted or not.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && ActivityCompat.checkSelfPermission
+                (getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+            //After this point you wait for callback in onRequestPermissionsResult(int, String[], int[]) overriden method
+        }
+
+        btnTakePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                b = Instruments.takeScreenshotOfRootView(imgViewScreen);
+                imgViewScreen.setImageBitmap(b);
+
+//                saveBitmapToGallery(b, String.valueOf(System.currentTimeMillis()));
+            }
+        });
+
+        btnSavePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveBitmapToGallery(b, String.valueOf(System.currentTimeMillis()));
+            }
+        });
+    }
+
+    private void saveBitmapToGallery(Bitmap bitmap, String fName) {
+
+        // Check the SDK version and whether the permission is already granted or not.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && ActivityCompat.checkSelfPermission
+                (getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+            return;
+            //After this point you wait for callback in onRequestPermissionsResult(int, String[], int[]) overriden method
+        }
+
+        String root = Environment.getExternalStoragePublicDirectory(Environment
+                .DIRECTORY_PICTURES).toString();
+
+        File myDir = new File(root + "/Screen");
+        myDir.mkdirs();
+        fName += ".jpg";
+        File file = new File(myDir, fName);
+        if (!file.exists()) {
+            file.delete();
+        }
+        try {
+            FileOutputStream outputStream = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+            outputStream.flush();
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        MediaScannerConnection.scanFile(getActivity(), new String[]{file.toString()}, null, new
+                MediaScannerConnection.OnScanCompletedListener() {
+            @Override
+            public void onScanCompleted(String path, Uri uri) {
+            }
+        });
     }
 
     float[] valueAccelerometer = new float[3];
@@ -114,6 +192,8 @@ public class FragmentSensor extends Fragment {
         return String.format("%1$.1f\t\t%2$.1f\t\t%3$.1f", values[0], values[1],
                 values[2]);
     }
+
+
 
 
 
--- app/src/main/java/dmitry/com/facultativeapp/helpers/Instruments.java
+++ app/src/main/java/dmitry/com/facultativeapp/helpers/Instruments.java
@@ -1,8 +1,26 @@
 package dmitry.com.facultativeapp.helpers;
 
+import android.graphics.Bitmap;
+import android.view.View;
+
 public class Instruments {
 
+
+
     //Класс в котором располагаются воспомогательные методы
+
+    private static Bitmap takeScreenshot(View v) {
+        v.setDrawingCacheEnabled(true);
+        v.buildDrawingCache(true);
+        Bitmap b = Bitmap.createBitmap(v.getDrawingCache());
+        v.setDrawingCacheEnabled(false);
+
+        return b;
+    }
+
+    public static Bitmap takeScreenshotOfRootView(View v) {
+        return takeScreenshot(v.getRootView());
+    }
 
 
 
