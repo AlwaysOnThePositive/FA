c7f6dbc936f93fe2249f573da089ba1cbe86219c
AlwaysOnThePositive <43723172+AlwaysOnThePositive@users.noreply.github.com>
2018-12-13 22:12:39 +0300

    сделан сплешскрин

--- app/src/main/java/dmitry/com/facultativeapp/SplashActivity.java
+++ app/src/main/java/dmitry/com/facultativeapp/SplashActivity.java
@@ -1,19 +1,20 @@
 package dmitry.com.facultativeapp;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class ActivitySplash extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         Intent intent = new Intent(this, ActivityAuth.class);
         startActivity(intent);
         finish();
     }
+
 
 }
